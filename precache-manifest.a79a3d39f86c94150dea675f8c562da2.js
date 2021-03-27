af32ec441131fc52d58abf7a6963ab86f31edd4e
Divesh <diveshanand9110@gmail.com>
2021-03-27 17:40:45 +0530

    Updates

---
+++ precache-manifest.a79a3d39f86c94150dea675f8c562da2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9286d470ddbc4c37751b166fc61a6dd",
+    "url": "/meesho-test/index.html"
+  },
+  {
+    "revision": "e38e307059b968757fa6",
+    "url": "/meesho-test/static/css/4.4af1d344.chunk.css"
+  },
+  {
+    "revision": "79510f68a22f9fb116be",
+    "url": "/meesho-test/static/css/main.b9aa184c.chunk.css"
+  },
+  {
+    "revision": "e38e307059b968757fa6",
+    "url": "/meesho-test/static/js/4.cdf29630.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meesho-test/static/js/4.cdf29630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00d73c3561a983fcda66",
+    "url": "/meesho-test/static/js/5.dbe4ced8.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/meesho-test/static/js/5.dbe4ced8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1e7576ee4f981ccb77",
+    "url": "/meesho-test/static/js/6.a1912ff3.chunk.js"
+  },
+  {
+    "revision": "79510f68a22f9fb116be",
+    "url": "/meesho-test/static/js/main.9ec204da.chunk.js"
+  },
+  {
+    "revision": "add059529b0d19e8ada0",
+    "url": "/meesho-test/static/js/runtime-main.1511d438.js"
+  },
+  {
+    "revision": "dcb283be28604fa49b00",
+    "url": "/meesho-test/static/js/stencil-h8k-logo_2-entry-js.91b3f4dd.chunk.js"
+  },
+  {
+    "revision": "8e1ab548c4a73a0aab4a",
+    "url": "/meesho-test/static/js/stencil-polyfills-css-shim.db56049d.chunk.js"
+  }
+]);
